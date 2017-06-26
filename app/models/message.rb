3083c24e58ba37ce030cfefb1954e0eaa204e108
Nathan Lee <kwnath@gmail.com>
2017-06-26 19:06:44 +0800

    Added Messages Associations

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,5 @@
 class Message < ApplicationRecord
+  belongs_to :sender, class_name: "User", foreign_key: "sender_id"
+  belongs_to :recipient, class_name: "User",foreign_key: "recipient_id"
+  # scope: :unread, -> { where read: false }
 end
