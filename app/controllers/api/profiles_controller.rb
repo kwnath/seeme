6131c2b07ea1f49219756dadf72814fb9a1aeb33
Nathan Lee <kwnath@gmail.com>
2017-06-26 17:32:40 +0800

    Updated routes to include API namespace

---
+++ app/controllers/api/profiles_controller.rb
@@ -0,0 +1,10 @@
+class Api::ProfilesController < ApplicationController
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :users, only: [:index, :show]
+    resources :meetings do
+      resources :messages, only: [:index, :new, :create]
+    end
+    resource :profile, only: [:edit, :show, :update]
+  end
 end
