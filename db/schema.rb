d52feeba70b9a7a7b2eda8eaee072ac6f77db2d6
Nathan Lee <kwnath@gmail.com>
2017-06-29 21:00:50 +0800

    made user gender integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170629125738) do
+ActiveRecord::Schema.define(version: 20170629130033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,7 +77,7 @@ ActiveRecord::Schema.define(version: 20170629125738) do
     t.string   "avatar"
     t.boolean  "admin",                             default: false
     t.string   "bio"
-    t.string   "gender"
+    t.integer  "gender"
     t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
