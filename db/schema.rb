3b15a56d709464cece4c57a95e662a1cc0504dde
Nathan Lee <kwnath@gmail.com>
2017-06-29 20:32:15 +0800

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170628073735) do
+ActiveRecord::Schema.define(version: 20170629002917) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 20170628073735) do
     t.string   "language"
     t.string   "avatar"
     t.boolean  "admin",                             default: false
+    t.string   "bio"
     t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
