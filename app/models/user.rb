d2193fcfd6fa3fabc9b5cc4a649f59f740fad369
Jay <jay.gao@139.com>
2017-06-30 16:23:08 +0800

    profile update

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ class User < ApplicationRecord
   has_many :messages, class_name: "Message", foreign_key: "recipient_id"
   has_many :sent_messages, class_name: "Message", foreign_key: "sender_id"
 
+  acts_as_taggable
   acts_as_taggable_on :interests
 #   has_many :sender_meetings, class_name: "Meeting", foreign_key: "sender_id"
 #   has_many :recipient_meetings, class_name: "Meeting", foreign_key: "sender_id"
