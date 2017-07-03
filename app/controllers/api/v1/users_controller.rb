c335e02eecae6218f42db2c8c10451da90692a4e
etienne pelletier gagne <etienne.pelletier.gagne0@gmail.com>
2017-07-03 16:45:35 +0800

    calculation for geo

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,13 +3,57 @@ class Api::V1::UsersController < Api::V1::BaseController
   acts_as_token_authentication_handler_for User, except: [ :show, :index, :create ]
 
 
+
   before_action :set_user, only: [ :show ]
 
+
   # GET /users
+  # def index
+  #   @users = policy_scope(User)
+  #   # authorize @user
+  # end
+
   def index
-    @users = policy_scope(User)
-    # authorize @user
+  @users = policy_scope(User)
+
+  lat1 = @current_user.lat
+  lng1 = @current_user.lng
+
+  # loc_current = []
+  # loc_user = []
+  # loc_current << lat1
+  # loc_current << lng1
+
+  rad_per_deg = Math::PI/180  # PI / 180
+  rkm = 6371                  # Earth radius in kilometers
+  r = 6371000                # Earth radius in meters
+
+
+  @users.select do |u|
+
+    lat2 = u.lat
+    lng2 = u.lng
+
+    lat_1_rad = lat1 * rad_per_deg
+    lat_2_rad = lat2 * rad_per_deg
+
+    dlat_rad = (lat2 - lat1) * rad_per_deg
+    dlon_rad = (lng2 - lng1) * rad_per_deg
+
+
+    a = Math.sin(dlat_rad / 2) * Math.sin(dlat_rad / 2) + Math.cos(lat_1_rad) * Math.cos(lat_2_rad) * Math.sin(dlon_rad/2) * Math.sin(dlon_rad/2)
+
+    c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
+
+    d = r * c
+
+    d < 3 ? u = true : u = false
+
   end
+
+
+  end
+
 
   # GET /users/:id
 
