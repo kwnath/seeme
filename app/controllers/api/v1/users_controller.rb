5ead3cc3b846b9a980e40ed3098162c2eedb68b9
Nathan Lee <kwnath@gmail.com>
2017-07-04 16:06:33 +0800

    added end tag for controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -65,7 +65,12 @@ class Api::V1::UsersController < Api::V1::BaseController
       skip_authorization
 
   # render json: @nearby_users
-render json: @lng
+render :json => {
+      :nearby_users => @nearby_users,
+      :lat => @lat,
+      :lng => @lng,
+   }
+ end
 
 
   # GET /users/:id
