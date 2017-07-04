0fc552931ff0aac5ee095519a1bdf321652df12b
Nathan Lee <kwnath@gmail.com>
2017-07-04 19:06:19 +0800

    added nearby_users array

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -31,30 +31,31 @@
   rad_per_deg = Math::PI/180  # PI / 180
   rkm = 6371                  # Earth radius in kilometers
   r = 6371000                # Earth radius in meters
+  @nearby_users = []
 
-  # @users.each do |u|
+  @users.each do |u|
 
-  #   lat2 = u.lat
-  #   lng2 = u.lng
+    lat2 = u.lat
+    lng2 = u.lng
 
-  #   lat_1_rad = @lat * rad_per_deg
-  #   lat_2_rad = lat2 * rad_per_deg
+    lat_1_rad = @lat * rad_per_deg
+    lat_2_rad = lat2 * rad_per_deg
 
-  #   dlat_rad = (lat2 - @lat) * rad_per_deg
-  #   dlon_rad = (lng2 - lng) * rad_per_deg
+    dlat_rad = (lat2 - @lat) * rad_per_deg
+    dlon_rad = (lng2 - lng) * rad_per_deg
 
 
-  #   a = Math.sin(dlat_rad / 2) * Math.sin(dlat_rad / 2) + Math.cos(lat_1_rad) * Math.cos(lat_2_rad) * Math.sin(dlon_rad/2) * Math.sin(dlon_rad/2)
+    a = Math.sin(dlat_rad / 2) * Math.sin(dlat_rad / 2) + Math.cos(lat_1_rad) * Math.cos(lat_2_rad) * Math.sin(dlon_rad/2) * Math.sin(dlon_rad/2)
 
-  #   c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
-  #   d = (r * c).round(2)
-  #   puts "------------------ Distance ---------------------"
-  #   puts d
-  #   # distance is in km
-  #     if d <= 3000
-  #       @nearby_users << {'user' => u, 'distance' => d}
-  #     end
-  #   end
+    c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
+    d = (r * c).round(2)
+    puts "------------------ Distance ---------------------"
+    puts d
+    # distance is in km
+      if d <= 3000
+        @nearby_users << {'user' => u, 'distance' => d}
+      end
+    end
     skip_authorization
     render json: @lat
   end
