b0d212ec8d3c21c70c6ac0e921cce67297eb037d
Nathan Lee <kwnath@gmail.com>
2017-07-04 18:51:00 +0800

    added response

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -17,7 +17,7 @@
   @users = policy_scope(User)
   # @users = User.all
  puts "these are params"
-  # @response = JSON.parse(response)
+  @response = JSON.parse(response)
   lat1 = params['lat']
   lng1 = params['lng']
 
@@ -34,31 +34,31 @@
   r = 6371000                # Earth radius in meters
   @nearby_users = []
 
-  @users.each do |u|
+  # @users.each do |u|
 
-    lat2 = u.lat
-    lng2 = u.lng
+  #   lat2 = u.lat
+  #   lng2 = u.lng
 
-    lat_1_rad = lat * rad_per_deg
-    lat_2_rad = lat2 * rad_per_deg
+  #   lat_1_rad = lat * rad_per_deg
+  #   lat_2_rad = lat2 * rad_per_deg
 
-    dlat_rad = (lat2 - lat) * rad_per_deg
-    dlon_rad = (lng2 - lng) * rad_per_deg
+  #   dlat_rad = (lat2 - lat) * rad_per_deg
+  #   dlon_rad = (lng2 - lng) * rad_per_deg
 
 
-    a = Math.sin(dlat_rad / 2) * Math.sin(dlat_rad / 2) + Math.cos(lat_1_rad) * Math.cos(lat_2_rad) * Math.sin(dlon_rad/2) * Math.sin(dlon_rad/2)
+  #   a = Math.sin(dlat_rad / 2) * Math.sin(dlat_rad / 2) + Math.cos(lat_1_rad) * Math.cos(lat_2_rad) * Math.sin(dlon_rad/2) * Math.sin(dlon_rad/2)
 
-    c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
-    d = (r * c).round(2)
-    puts "------------------ Distance ---------------------"
-    puts d
-    # distance is in km
-      if d <= 3000
-        @nearby_users << {'user' => u, 'distance' => d}
-      end
-    end
+  #   c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
+  #   d = (r * c).round(2)
+  #   puts "------------------ Distance ---------------------"
+  #   puts d
+  #   # distance is in km
+  #     if d <= 3000
+  #       @nearby_users << {'user' => u, 'distance' => d}
+  #     end
+  #   end
     skip_authorization
-    render json: @nearby_users
+    render json: @response
    end
 
  def search
