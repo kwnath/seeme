92bb288ea51476462758205f3246c14131e41eec
Nathan Lee <kwnath@gmail.com>
2017-07-04 17:39:59 +0800

    added authorise

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -8,8 +8,8 @@ class Api::V1::ProfilesController < Api::V1::BaseController
 
   def update
     @user = current_user
-    # authorize @user
-    skip_authorization
+    authorize @user
+    # skip_authorization
     @user.update(profile_params)
     render :show
   end
