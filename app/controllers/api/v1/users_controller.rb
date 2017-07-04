c833b6f735acdd25725f87c6e1a366a9522bb4d0
Nathan Lee <kwnath@gmail.com>
2017-07-04 15:23:31 +0800

    Added back geolocation formula

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -19,49 +19,48 @@ class Api::V1::UsersController < Api::V1::BaseController
  puts "these are params"
 
   # @response = JSON.parse(response)
-  @lat = params['lat']
-  # lng = response['lng']
+  lat = params['lat']
+  lng = params['lng']
 
   # # lat = BigDecimal.new(lat1)
   # # lng = BigDecimal.new(lng1)
 
-  # loc_current = []
-  # loc_user = []
-  # loc_current << lat
-  # loc_current << lng
+  loc_current = []
+  loc_user = []
+  loc_current << lat
+  loc_current << lng
 
-  # rad_per_deg = Math::PI/180  # PI / 180
-  # rkm = 6371                  # Earth radius in kilometers
-  # r = 6371000                # Earth radius in meters
-  # @nearby_users = []
+  rad_per_deg = Math::PI/180  # PI / 180
+  rkm = 6371                  # Earth radius in kilometers
+  r = 6371000                # Earth radius in meters
+  @nearby_users = []
 
-  # @users.each do |u|
+  @users.each do |u|
 
-  #   lat2 = u.lat
-  #   lng2 = u.lng
+    lat2 = u.lat
+    lng2 = u.lng
 
-  #   lat_1_rad = lat * rad_per_deg
-  #   lat_2_rad = lat2 * rad_per_deg
+    lat_1_rad = lat * rad_per_deg
+    lat_2_rad = lat2 * rad_per_deg
 
-  #   dlat_rad = (lat2 - lat) * rad_per_deg
-  #   dlon_rad = (lng2 - lng) * rad_per_deg
+    dlat_rad = (lat2 - lat) * rad_per_deg
+    dlon_rad = (lng2 - lng) * rad_per_deg
 
 
-  #   a = Math.sin(dlat_rad / 2) * Math.sin(dlat_rad / 2) + Math.cos(lat_1_rad) * Math.cos(lat_2_rad) * Math.sin(dlon_rad/2) * Math.sin(dlon_rad/2)
+    a = Math.sin(dlat_rad / 2) * Math.sin(dlat_rad / 2) + Math.cos(lat_1_rad) * Math.cos(lat_2_rad) * Math.sin(dlon_rad/2) * Math.sin(dlon_rad/2)
 
-  #   c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
-  #   d = (r * c).round(2)
-  #   puts "------------------ Distance ---------------------"
-  #   puts d
-  #   # distance is in km
-  #   if d <= 3000
-  #     @nearby_users << {'user' => u, 'distance' => d}
-  #   end
-  # end
-  #   # skip_authorization
-  # # render json: @nearby_users
+    c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
+    d = (r * c).round(2)
+    puts "------------------ Distance ---------------------"
+    puts d
+    # distance is in km
+    if d <= 3000
+      @nearby_users << {'user' => u, 'distance' => d}
+    end
+  end
+    # skip_authorization
   # render json: @nearby_users
-  render json: @lat
+  render json: @nearby_users
   end
 
   # GET /users/:id
