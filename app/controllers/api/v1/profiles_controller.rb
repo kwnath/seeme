8382e2e0c3dc2a57a467f6ed7c67afe221281e19
Nathan Lee <kwnath@gmail.com>
2017-07-04 16:29:33 +0800

    removed authorization

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -8,10 +8,10 @@ class Api::V1::ProfilesController < Api::V1::BaseController
 
   def update
     @user = current_user
-    # authorize @user
-    skip_authorization
+    authorize @user
+    # skip_authorization
     @user.update(profile_params)
-    render json: @user
+    render :show
   end
 
   private
